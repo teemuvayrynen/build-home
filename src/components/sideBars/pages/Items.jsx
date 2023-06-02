666ef9f697b491a5aa560c48c3677c9da08c963f
Teemu Väyrynen <85222095+teemuvayrynen@users.noreply.github.com>
2023-06-02 15:22:21 +0300

    ei mitään ihmeellistä

--- src/components/sideBars/pages/Items.jsx
+++ src/components/sideBars/pages/Items.jsx
@@ -1,8 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Container, ItemContainer, Header } from "./StyledFunctions.jsx"
+
 
 export default function Items() {
+
   return (
-    <div>
-      <h1>Items</h1>
-    </div>
+    <Container>
+      <ItemContainer>
+        <Header>Rooms</Header>
+
+      </ItemContainer>
+      <ItemContainer>
+        <Header>Wall Elements</Header>
+
+      </ItemContainer>
+      <ItemContainer>
+        <Header>Furniture</Header>
+
+      </ItemContainer>
+    </Container>
   )
 }
